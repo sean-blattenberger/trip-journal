910824fab15b6113080a08ccd57d8d9d50f07a90
sean-blattenberger <sean.blattenberger@gmail.com>
2018-06-20 15:12:56 -0600

    fixed dbconnection path

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const knex = require("./database-connection");
+const knex = require("../../database-connection");
 
 router.post("/", (req, res, next) => {
   return knex("note")
